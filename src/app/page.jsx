6d71ba5fec791e9bf68c531e886c58570146b958
Akash-Ahamed <akash.ahamed.engr@gmail.com>
2025-01-07 14:49:37 +0600

    Build a user interface using react component that renders the homepage of 'Expense Tracker' dashboard, displaying available funds and provide navigation option for various functionalities like adding funds, expenses, viewing reports and future predictions. The main function (Home) uses the useFetchFunds hook to get the funds value. Renders the dashboard structure using a combination of div containers styled with utility classes (possibly Tailwind CSS).

---
+++ src/app/page.jsx
@@ -0,0 +1,38 @@
+"use client";
+import Link from "next/link";
+import { useFetchFunds } from "@/hooks/useFetchFunds";
+export default function Home() {
+  const funds = useFetchFunds();
+  return (
+    <div className="items-center justify-items-center p-10">
+      {/* Title of the Dashboard */}
+      <h1 className="text-5xl font-bold">Expense Tracker</h1>
+      <div>
+        <h2 className="text-4xl font-bold">Available Funds</h2>
+        <p className="text-4xl font-bold">TK {funds}</p>
+      </div>
+
+      {/* Button Add Fund, Expenses Report, Pie Chart Report, Future Prediction */}
+      <div className="flex justify-between items-center px-6 py-6 container">
+        <div className="bg-blue-500 font-bold text-2xl p-2 text-white">
+          <Link href={"/addFund"}>Add Fund</Link>
+        </div>
+
+        <div className="bg-blue-500 font-bold text-2xl p-2 text-white">
+          <Link href={"/addExpense"}>Add Expense</Link>
+        </div>
+        <div className="bg-blue-500 font-bold text-2xl p-2 text-white">
+          <Link href={"/expenseReport"}>Expenses Report</Link>
+        </div>
+        <div className="bg-blue-500 font-bold text-2xl p-2 text-white">
+          <Link href={"/expensePieChart"}>Pie Chart Report</Link>
+        </div>
+        <div className="bg-blue-500 font-bold text-2xl p-2 text-white">
+          <Link href={"/futurePrediction"}>Future Prediction</Link>
+        </div>
+      </div>
+    </div>
+
+    // Options for selection
+  );
+}
